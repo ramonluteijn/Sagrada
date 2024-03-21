f01c4370927cb09636db931dc543795502449828
ramon luteijn <ramon@intractief.nl>
2024-03-21 12:46:25 +0100

    main

---
+++ Main.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Sagrada");
+        stage.setScene(new Scene(new Pane()));
+        stage.show();
+    }
+}
