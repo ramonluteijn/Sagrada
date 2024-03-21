ce4e3b99d772671a19c99911fa0949743f4e99b8
ramon luteijn <ramon@intractief.nl>
2024-03-21 18:29:44 +0100

    dbconn

--- Main.java
+++ Main.java
@@ -3,17 +3,23 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+//        launch(args);
+        DBConn conn = new DBConn();
+        if (conn.loadDataBaseDriver("com.mysql.cj.jdbc.Driver")
+                && (conn.makeConnection()))
+        {
+            conn.query("select * from hemelobject");
+        }
     }
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        stage.setTitle("Sagrada");
-        stage.setScene(new Scene(new Pane()));
-        stage.show();
-    }
+//    @Override
+//    public void start(Stage stage) throws Exception {
+//        stage.setTitle("Sagrada");
+//        stage.setScene(new Scene(new Pane()));
+//        stage.show();
+//    }
 }
 
 
