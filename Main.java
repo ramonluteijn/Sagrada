5ef566883bd7808c743300cb49848cc4e43f1dbb
joepss <150429156+joepss@users.noreply.github.com>
2024-04-09 20:14:02 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -15,7 +15,7 @@ public class Main extends Application{
 
 //    @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("jow");
+        stage.setTitle("Sagrada");
         stage.setScene(new MyScene());
         stage.show();
     }
