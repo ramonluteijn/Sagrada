bf04de39b49cecb930dd791e44cecbe501da33b3
BrinkySaus <120514288+BrinkySaus@users.noreply.github.com>
2024-04-10 19:36:00 +0200

    MainController
    MainController + alle andere controllers, maar dit zijn nog lege classes.
    de maincontroller word nu ook in de main geïnstancieert.
    de main is ook compleet nu. (met alles erin en niks weg gehaalt. dus bijv de dbconn zit er ook in)

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -1,4 +1,5 @@
 package Controller;
+import Database.DBConn;
 import View.MyScene;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -6,11 +7,14 @@ import javafx.stage.Stage;
 public class Main extends Application{
     public static void main(String[] args) {
         launch(args);
-//        DBConn conn = new DBConn("com.mysql.cj.jdbc.Driver");
+        DBConn conn = new DBConn("com.mysql.cj.jdbc.Driver");
     }
 
 //    @Override
     public void start(Stage stage) throws Exception {
+    	
+    	MainController mainController = new MainController();
+    	
         stage.setTitle("Sagrada");
         stage.setScene(new MyScene());
         stage.show();
