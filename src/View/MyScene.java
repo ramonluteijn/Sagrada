f6d42a3a98d008ce9a2db9ba2a3ab47645e5094c
Keranus <164501208+Keranus@users.noreply.github.com>
2024-04-16 14:03:00 +0200

    favortokensview gemaakt
    favortokensview afgemaakt, create- en drawFavortokens toegevoegd. Create is nodig voor de draw, niet om echte favortokens aan te maken. Liefs, Bram

--- src/View/MyScene.java
+++ src/View/MyScene.java
@@ -8,9 +8,15 @@ public class MyScene extends Scene {
     private Pane root;
     public MyScene() {
         super(new Pane());
+        
+        FavorTokensView tokens = new FavorTokensView();
+        
         root = new Pane();
-        root.getChildren().add(new SearchAccountView(this));
-
+        root.getChildren().add(tokens);
+ 
+        tokens.createFavortoken(1);
+        tokens.drawFavortokens();
+        
         this.setRoot(root);
     }
 }
