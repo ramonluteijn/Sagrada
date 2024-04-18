9e73f5e18ccfc990e17ad6f8df40c198bd24a0a0
Tommy030 <65161645+Tommy030@users.noreply.github.com>
2024-04-18 11:37:44 +0200

    DiceController +rollAllDices() Added

---
+++ src/Controller/DiceController.java
@@ -0,0 +1,20 @@
+package Controller;
+
+public class DiceController {
+
+     //private List<DieModel> dicesOutBag = new ArrayList<DieModel>();
+
+    public DiceController (){
+
+    }
+
+
+    public void rollAllDices(){
+        /*
+        for (DieModel dice : dicesOutBag) {
+            dice.rollDice();
+        }
+         */
+
+    }
+}
