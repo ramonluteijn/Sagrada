8d785132cc7ea46b0350e2a76c00230cb1c071a6
joepss <150429156+joepss@users.noreply.github.com>
2024-04-24 10:22:38 +0200

    ScoreView
    met een mooie font

--- src/View/ScoreView.java
+++ src/View/ScoreView.java
@@ -1,9 +1,37 @@
 package View;
 
-import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Screen;
 
-public class ScoreView extends Pane{
+public class ScoreView extends HBox{
 
+	int testscore = 10;
 	
+	public ScoreView() {
+
+		double screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
+        double screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
+        setPrefSize(screenWidth, screenHeight);
+        
+        this.getChildren().add(this.setupView());
+        
+
+        
+	}
 	
+	private Label setupView() {
+		
+		Label ownScore = new Label("SCORE: " + testscore);
+		Font font = Font.font("Verdana", FontWeight.BOLD, FontPosture.ITALIC, 35);
+		
+		ownScore.setPrefSize(400, 50);
+		ownScore.setFont(font);
+		
+		
+		return ownScore;
+	}
 }
